141ea6b6c5751e5afac2703eb35e54e60c02fd24
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-02 12:01:23 -0700

    header pic added

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import LoginView from "./view/LoginViews.tsx";
 import RegisterViews from "./view/RegisterViews.tsx";
+import AuthLayout from "./layout/AuthLayout.tsx";
 
 export default function Router(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route>
-                    <Route path='/auth/login' element={<LoginView/>}></Route>
-                    <Route path='/auth/register' element={<RegisterViews/>}></Route>
+                <Route element={<AuthLayout />}>
+                    <Route path='/auth/login' element={<LoginView />}></Route>
+                    <Route path='/auth/register' element={<RegisterViews />}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
--- src/view/LoginViews.tsx
+++ src/view/LoginViews.tsx
@@ -3,7 +3,6 @@ import {Link} from 'react-router-dom'
 export default function LoginViews(){
     return(
         <>
-            <div>Login</div>
             <nav>
                 <Link to='/auth/register'>
                     No tienes cuenta? registrate aqui
