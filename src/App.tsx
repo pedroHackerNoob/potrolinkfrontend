bcd7de837a19c7fd73fa93ec0e1f6d471b0cf3ec
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-02 09:36:12 -0700

    default proyect

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
 
   return (
     <>
-        <h1>xd xd</h1>
+        <h1 className='text-6xl '>hola tierra</h1>
     </>
   )
 }
