c93768f3f80c643b273fd941765efed241846fa7
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-02 11:21:59 -0700

    links added

--- src/view/LoginViews.tsx
+++ src/view/LoginViews.tsx
@@ -1,6 +1,14 @@
+import {Link} from 'react-router-dom'
 
 export default function LoginViews(){
     return(
-        <div>login View</div>
+        <>
+            <div>Login</div>
+            <nav>
+                <Link to='/auth/register'>
+                    No tienes cuenta? registrate aqui
+                </Link>
+            </nav>
+        </>
     )
 }
--- src/view/RegisterViews.tsx
+++ src/view/RegisterViews.tsx
@@ -1,6 +1,14 @@
+import {Link} from "react-router-dom";
 
 export default function RegisterViews(){
     return(
-        <div>Register View</div>
+        <>
+            <div>Register</div>
+            <nav>
+                <Link to='/auth/login'>
+                    ya tienes cuenta? inicia sesion aqui
+                </Link>
+            </nav>
+        </>
     )
 }
