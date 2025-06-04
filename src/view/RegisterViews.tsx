14961e42c345b17c662e5030ce2d38faa8805297
pedroHackerNoob <114507828+pedroHackerNoob@users.noreply.github.com>
2025-06-03 18:29:16 -0700

    url save content added

--- src/view/RegisterViews.tsx
+++ src/view/RegisterViews.tsx
@@ -2,8 +2,9 @@ import {Link} from "react-router-dom";
 import {useForm} from "react-hook-form";
 import ErrorMessage from "../components/ErrorMessage.tsx";
 import type {RegisterForm} from '../types'
-import axios,{isAxiosError} from "axios";
+import {isAxiosError} from "axios";
 import {toast} from 'sonner'
+import api from '../config/axios.ts'
 
 
 export default function RegisterViews(){
@@ -19,7 +20,7 @@ export default function RegisterViews(){
 
     const handleRegister = async (formData : RegisterForm) => {
         try {
-            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`,formData)
+            const {data} = await api.post(`/auth/register`,formData)
             toast.success(data)
             console.log(data)
             // reset()
